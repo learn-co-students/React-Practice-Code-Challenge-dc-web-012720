130db8419fa877e386e0d620195b2b896a37dbe7
matteoaricci <matteoaricci@DESKTOP-KID7KUE.localdomain>
2020-04-05 14:07:55 -0400

    more button works

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,11 +6,36 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  
+  constructor(){
+    super();
+    this.state = {
+      allSushi: [],
+      display_index: 0
+    }
+  }
+  
+  componentDidMount(){
+    fetch(API)
+    .then(resp => resp.json())
+    .then(sushi => this.setState({ allSushi: sushi}))
+    
+  }
+
+  showSushi = () => {
+        return this.state.allSushi.slice(this.state.display_index, this.state.display_index+4)
+  }
+
+  updateDisplayIndex = () => {
+    this.setState({
+      display_index: this.state.display_index + 4
+    })
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
+        <SushiContainer allSushi={this.showSushi()} btnFunction = {this.updateDisplayIndex}/>
         <Table />
       </div>
     );
