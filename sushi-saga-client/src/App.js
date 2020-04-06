391654cdd2f979642ac03936aba78296fa7656ab
github username <github email address>
2020-04-06 12:02:53 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,20 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
-class App extends Component {
+export default function App() {
 
-  render() {
-    return (
-      <div className="app">
-        <SushiContainer />
-        <Table />
-      </div>
-    );
+  let [sushiList, sushiListSet] = useState([]);
+  let [removedSushi, removedSushiSet] = useState([])
+  let [total, totalSet] = useState(100)
+
+
+
+  useEffect(() => {
+    function fetchSushi() {
+      fetch(API).then(r => r.json()).then(json => sushiListSet(json))
+    }
+    fetchSushi()
+  }, [])
+
+  function removeASushi(data) {
+    removedSushiSet(removedSushi.concat(data));
   }
-}
 
-export default App;
+  return (
+    <div className="app">
+      <SushiContainer sushiList={sushiList} removeASushi={removeASushi} total={total} totalSet={totalSet} />
+      <Table eatenSushi={removedSushi} total={total} />
+    </div>
+  );
+
+
+
+}
--- sushi-saga-client/src/containers/Table.js
+++ sushi-saga-client/src/containers/Table.js
@@ -1,27 +1,24 @@
 import React, { Fragment } from 'react'
 
-const Table = (props) => {
+const Table = ({ eatenSushi, total, totalSet }) => {
 
   const renderPlates = (array) => {
     return array.map((x, index) => {
-      return <div className="empty-plate" style={{ top: -7 * index }}/>
+      return <div className="empty-plate" style={{ top: -7 * index }} key={index} />
     })
   }
+
+
 
   return (
     <Fragment>
       <h1 className="remaining">
-        You have: ${ /* Give me how much money I have left */ } remaining!
+        You have: ${total} remaining!
       </h1>
       <div className="table">
         <div className="stack">
           {
-            /* 
-               renderPlates takes an array 
-               and renders an empty plate
-               for every element in the array
-            */
-            renderPlates([])
+            renderPlates(eatenSushi)
           }
         </div>
       </div>
